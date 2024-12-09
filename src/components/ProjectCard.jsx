dbe858f6308e1f90e96c030b92c1a0fa49720209
javiscripto <javier.mecker94@gmail.com>
2024-12-09 17:51:30 -0300

    agrego data a project.json y agrego iframe de youtube a componente projectlistcontainer

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,31 +1,25 @@
-import React from 'react'
-import styles from './Card.module.css'
+import React from 'react';
+import styles from './Card.module.css';
 import { ScrollRevealEffect } from './efects/ScrollrevealEffect';
 
-
-
-
-export const Projectcard = ({ project }) => {
-  const { title, description, img } = project;
+export const Projectcard = ({ project, handleToggleVysybility }) => {
+  const { title, description, Url } = project;
 
   return (
     <ScrollRevealEffect>
       <div className={styles.cardContainer}>
-        <div className={`${styles.cardImg}, ${styles.projectCardImg}`} onClick={() => handleToggleVysybility(img, true)}>
-          <img src={img} alt={title} />
-
+        <div
+          className={`${styles.cardImg} ${styles.projectCardImg}`}
+          onClick={() => handleToggleVysybility(Url, true)}
+        >
+          {/* Aquí solo mostramos un fondo o placeholder */}
+          <p>Haz clic para ver</p>
         </div>
         <div className={styles.textContainer}>
           <h3 className="title">{title}</h3>
-          <p>
-            {description}
-          </p>
-
+          <p>{description}</p>
         </div>
       </div>
-
     </ScrollRevealEffect>
-
-  )
-}
-
+  );
+};
--- src/components/projectListContainer.jsx
+++ src/components/projectListContainer.jsx
@@ -1,24 +1,54 @@
 import { useEffect, useState } from 'react';
 import { Projectcard } from './ProjectCard';
-import styles from "./cardContainer.module.css"
+import styles from './cardContainer.module.css';
 import { fetchData } from '../helpers/fetchData';
+
 export const ProjectListContainer = () => {
   const [projects, setProjects] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [visibility, setVisibility] = useState(false);
+  const [Url, setUrl] = useState('');
 
   useEffect(() => {
     fetchData('/data/projects.json', setProjects, setLoading);
-
   }, []);
+
+  const handleToggleVysybility = (childUrl, childVisibility) => {
+    setUrl(childUrl);
+    setVisibility(childVisibility);
+  };
+
+  const handleCloseLightbox = () => {
+    setVisibility(false);
+    setUrl('');
+  };
 
   return (
     <>
-      {loading ? (<h3>Cargando...</h3>) : (
+      {visibility && Url && (
+        <div className={styles.lightboxImage}>
+          <div className={styles.overlay} onClick={handleCloseLightbox}></div>
+          <div className={styles.image}>
+            <iframe src={Url} width="100%" height="100%" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share ; allowfullscreen" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <button onClick={handleCloseLightbox} className={styles.closeButton}>
+              Cerrar
+            </button>
+          </div>
+        </div>
+      )}
+      {loading ? (
+        <h3>Cargando...</h3>
+      ) : (
         <div className={styles.container}>
           {projects.map((project) => (
-            <Projectcard key={project.title} project={project} />
+            <Projectcard
+              key={project.title}
+              project={project}
+              handleToggleVysybility={handleToggleVysybility}
+            />
           ))}
-        </div>)}
+        </div>
+      )}
     </>
   );
 };
